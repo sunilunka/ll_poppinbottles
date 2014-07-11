a9dfb531647eda4713c3285c9d495e54947b3885
Sunil Unka <sunil.unka@gmail.com>
2014-07-11 23:13:54 +0000

    Completed Task 2

--- main.rb
+++ main.rb
@@ -5,15 +5,23 @@ def get_bottles(money)
   from_bottles = bought/2 
   from_caps = bought/4
   total_free_bottles = from_bottles + from_caps
-  puts "#{bought}, #{total_free_bottles}"
+  puts "Buying $#{money} bottles means you can get #{total_free_bottles} when they are recycled."
 end
 
 def bottles_bought
   puts "How much money are you investing in pop?"
-  print "To spend $" 
+  print "To spend: $ " 
   spending_on_pop =  gets.chomp
   amount_to_compute = spending_on_pop.to_i
   get_bottles(amount_to_compute)
 end
 
-bottles_bought
+def run_program
+  status = 0
+  while status < 6
+    bottles_bought
+    status += 1
+  end
+end
+
+run_program
